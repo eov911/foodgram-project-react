0298c9f824becd70ce308ffb3655101fe21e15bc
Оксана Ерегина <oksanaeregina@gmail.com>
2023-05-11 01:00:26 +0300

    fix

--- backend/api/views.py
+++ backend/api/views.py
@@ -167,9 +167,7 @@ class RecipeViewSet(ModelViewSet):
             for ingredient in ingredients
         ])
         shopping_list += f'\n\nFoodgram ({today:%Y})'
-
         filename = f'{user.username}_shopping_list.txt'
         response = HttpResponse(shopping_list, content_type='text/plain')
         response['Content-Disposition'] = f'attachment; filename={filename}'
-
         return response
